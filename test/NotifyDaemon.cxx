d47fe5df19fca6ef7eb22f2c9fb2d9e5d2ae0a0f
Joel Schumacher <joel.schumacher@ionos.com>
2025-01-02 16:49:43 +0100

    Restructure to avoid concurrent queries

--- test/NotifyDaemon.cxx
+++ test/NotifyDaemon.cxx
@@ -1,3 +1,5 @@
+#include <queue>
+
 #include <fmt/core.h>
 
 #include <event/Loop.hxx>
@@ -11,21 +13,32 @@
 struct NotifyDaemon
   : Pg::AsyncConnectionHandler
   , Pg::AsyncResultHandler {
-	enum class State {
-		Init = 0,
+	enum class CurrentQuery {
+		None = 0,
 		Listen,
-		ProcessEvents,
+		ProcessEvent,
+		DeleteEvent,
+	};
+
+	struct Event {
+		long id;
+		std::string event;
+		std::string params;
 	};
 
 	EventLoop event_loop;
 	RootPool root_pool;
 	Pg::AsyncConnection conn;
 	std::string schema;
-	State state = State::Init;
+	std::string datacenter_id;
+	CurrentQuery current_query = {};
+	std::queue<Event> event_queue;
+	bool notified = false;
 
-	NotifyDaemon(const char *conninfo, const char *schema_)
+	NotifyDaemon(const char *conninfo, const char *schema_, std::string datacenter_id_)
 	  : conn(event_loop, conninfo, schema_, *this)
 	  , schema(schema_)
+	  , datacenter_id(std::move(datacenter_id_))
 	{
 		// PInstance
 #ifndef NDEBUG
@@ -44,30 +57,58 @@ struct NotifyDaemon
 
 	void Listen()
 	{
-		state = State::Listen;
 		std::string sql("LISTEN \"");
 		if (!schema.empty() && schema != "public") {
 			sql += schema;
 			sql += ':';
 		}
 		sql += "events_posted\"";
+		current_query = CurrentQuery::Listen;
 		conn.SendQuery(*this, sql.c_str());
 	}
 
 	void ProcessEvent()
 	{
-		std::string sql(R"(
+		notified = false;
+		const auto sql = R"(
 		UPDATE events SET
 		processed_at = NOW()
 		WHERE id = (
 			SELECT id FROM events
-			WHERE processed_at IS NULL
+			WHERE processed_at IS NULL AND datacenter_id = $1
 			FOR UPDATE SKIP LOCKED
 			LIMIT 1
 		)
-		RETURNING id;
-		)");
-		conn.SendQuery(*this, sql.c_str());
+		RETURNING id, event, params;
+		)";
+		current_query = CurrentQuery::ProcessEvent;
+		conn.SendQuery(*this, sql, datacenter_id);
+	}
+
+	void Notify(const Event &event)
+	{
+		fmt::print("notify: {}, {}\n", event.id, event.event);
+
+		// TODO: Actually send a notification
+
+		// Now completed, remove the event (alternatively set completed_at)
+		const auto sql = "DELETE FROM events WHERE id = $1";
+		current_query = CurrentQuery::DeleteEvent;
+		conn.SendQuery(*this, sql, event.id);
+	}
+
+	void Query()
+	{
+		if (current_query != CurrentQuery::None) {
+			return;
+		}
+		if (!event_queue.empty()) {
+			const auto event = event_queue.front();
+			event_queue.pop();
+			Notify(event);
+		} else if (notified) {
+			ProcessEvent();
+		}
 	}
 
 	void OnConnect() override // AsyncConnectionHandler
@@ -83,8 +124,11 @@ struct NotifyDaemon
 
 	void OnNotify(const char *name) override // AsyncConnectionHandler
 	{
+		// TODO: Put this event in a queue or something, because we shouldn't be calling ProcessEvent here,
+		// because we might already be processing another query in which case we must not call SendQuery again.
 		fmt::print("notify: {}\n", name);
-		ProcessEvent();
+		notified = true;
+		Query();
 	}
 
 	void OnError(std::exception_ptr e) noexcept override // AsyncConnectionHandler
@@ -95,18 +139,27 @@ struct NotifyDaemon
 	void OnResult(Pg::Result &&result) override // AsyncResultHandler
 	{
 		fmt::print("result status: {}\n", result.GetStatus());
-		if (state == State::ProcessEvents) {
+		if (current_query == CurrentQuery::ProcessEvent) {
 			const auto event_id = result.GetLongValue(0, 0);
-			fmt::print("event processed: {}\n", event_id);
+			const auto event = result.GetValueView(0, 1);
+			const auto params = result.GetValueView(0, 2);
+			// We can't just do the notification here, because it might finish before this query is done
+			// (OnResultEnd was called) and we must not call SendQuery before the current Query is finished.
+			event_queue.emplace(Event{ event_id, std::string(event), std::string(params) });
 		}
 	}
 
 	void OnResultEnd() override // AsyncResultHandler
 	{
-		if (state == State::Listen) {
+		if (current_query == CurrentQuery::Listen) {
 			fmt::print("Started listening\n");
-			state = State::ProcessEvents;
+		} else if (current_query == CurrentQuery::ProcessEvent) {
+			fmt::print("Processed event\n");
+		} else if (current_query == CurrentQuery::DeleteEvent) {
+			fmt::print("Completed event\n");
 		}
+		current_query = CurrentQuery::None;
+		Query();
 	}
 };
 
@@ -114,12 +167,13 @@ int
 main(int argc, char **argv)
 {
 	try {
-		if (argc != 2) {
-			fmt::print(stderr, "Usage: {} CONNINFO\n", argv[0]);
+		if (argc != 3) {
+			fmt::print(stderr, "Usage: {} CONNINFO DATACENTER\n", argv[0]);
 			return EXIT_FAILURE;
 		}
 		const auto conninfo = argv[1];
-		NotifyDaemon instance(conninfo, "");
+		const auto datacenter_id = argv[2];
+		NotifyDaemon instance(conninfo, "", datacenter_id);
 		instance.conn.Connect();
 		instance.event_loop.Run();
 		return EXIT_SUCCESS;
